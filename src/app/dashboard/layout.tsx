5e1b8438497fde9de062231645e35de0ce2cf2e0
Alexandre Vitorino Martins de Lima <alexandrevmartinsdelima@gmail.com>
2025-02-11 22:47:13 -0300

    style: update styling

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -13,7 +13,7 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     <div className="w-full h-screen overflow-hidden grid grid-cols-[300px,1fr]">
       <aside className="w-full h-full flex flex-col items-center border-r border-muted">
         <div className="w-full p-6 border-b border-muted">
-          <Logo className="max-w-[100px] mx-auto" />
+          <Logo className="max-w-[100px] mx-auto text-foreground" />
         </div>
 
         <NavItems />
